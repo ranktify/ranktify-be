4691598bcb4d9496c0ddf0a863dab5686b2e008d
print-Eruki <palejorniel@gmail.com>
2025-03-01 16:59:35 -0400

    Feat: Added CORS on localhost:8081

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"time"
+
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/ranktify/ranktify-be/config"
 	"github.com/ranktify/ranktify-be/internal/route"
@@ -8,6 +11,17 @@ import (
 
 func main() {
 	router := gin.Default()
+
+	// below is the setup cors for browser testing, this does not work with mobile devices
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:8081"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour, // Preflight request caching
+	}))
+
 	router.RemoveExtraSlash = true
 	db := config.SetupConnection()
 
@@ -16,5 +30,5 @@ func main() {
 		route.UserRoutes(mainGroup, db)
 	}
 
-	router.Run("localhost:8080")
+	router.Run("0.0.0.0:8080")
 }
