ca32f8aa5a72661b25257187fa7a4a00804ee11c
print-Eruki <palejorniel@gmail.com>
2025-04-10 00:46:56 -0400

    Added jwt auth to the spotify callback route

--- internal/route/api.go
+++ internal/route/api.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/ranktify/ranktify-be/internal/dao"
 	"github.com/ranktify/ranktify-be/internal/handler"
+	"github.com/ranktify/ranktify-be/internal/middleware"
 )
 
 func ApiRoutes(router *gin.RouterGroup, db *sql.DB) {
@@ -15,6 +16,9 @@ func ApiRoutes(router *gin.RouterGroup, db *sql.DB) {
 	api := router.Group("/api")
 	{
 		api.POST("/refresh", tokensHandler.Refresh)
+
+		//protected routes
+		api.Use(middleware.AuthMiddleware())
 		api.POST("/callback", spotifyHandler.AuthCallback)
 	}
 }
