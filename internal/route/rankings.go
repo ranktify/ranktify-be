1ec7c9645faa5f8a362da74587bebcdcdcb47b21
jorge.torres33 <jorge.torres33@upr.edu>
2025-04-20 01:59:32 -0400

    Added Authentication Middleware

--- internal/route/rankings.go
+++ internal/route/rankings.go
@@ -6,14 +6,16 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/ranktify/ranktify-be/internal/dao"
 	"github.com/ranktify/ranktify-be/internal/handler"
+	"github.com/ranktify/ranktify-be/internal/middleware"
 )
 
 func RankingsRoutes(group *gin.RouterGroup, db *sql.DB) {
 	rankingsDAO := dao.NewRankingsDAO(db)
 	rankingsHandler := handler.NewRankingsHandler(rankingsDAO)
-
+	
 	rankings := group.Group("/rankings")
 	{
+		rankings.Use(middleware.AuthMiddleware())
 		rankings.GET("/:user_id", rankingsHandler.GetRankedSongs)
 	}
 }
