b2df6ae4c172d1e2f1da14dd52c1b6ccad68a7af
print-Eruki <palejorniel@gmail.com>
2025-05-09 21:08:42 -0400

    Completed streak reset for cron job

--- scripts/streaks_reset.go
+++ scripts/streaks_reset.go
@@ -1,14 +1,23 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"log"
 
 	"github.com/ranktify/ranktify-be/config"
+	"github.com/ranktify/ranktify-be/internal/dao"
 )
 
 func main() {
 	db := config.SetupConnection()
 
 	// reset the streaks with dao methods to be develop
-	fmt.Println(db)
+	streaksDAO := dao.NewStreaksDAO(db)
+
+	err := streaksDAO.ResetStreaksDaily(context.Background())
+	if err != nil {
+		log.Println("Couldn't reset streaks, error:", err.Error())
+	}
+
+	log.Println("Reset streaks successfully")
 }
